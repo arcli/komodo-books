4aa3d7020296857715320b7409a376c072ebe835
Seth Ladygo <alx-git@arclight.industries>
2023-10-23 07:44:02 +0000

    Add `packages/models` with initial prisma schema and seed data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,22 @@
+import { PrismaClient } from "@prisma/client";
 import express, { Express, Request, Response } from "express";
 
+const prisma = new PrismaClient();
 const app: Express = express();
 const port = 8000;
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("foo");
+});
+
+app.get("/test", async (req, res) => {
+  const users = await prisma.user.findMany({
+    // where: { published: true },
+    // include: { author: true },
+  });
+  res.json(users);
 });
 
 app.listen(port, () => {
---
+++ packages/models/prisma/seed.ts
@@ -0,0 +1,72 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+import { default as users } from "./users.json";
+import { default as books } from "./books.json";
+
+// (1-based) array index becomes list id
+const listData = [
+  {
+    name: "Speculative fiction",
+    userId: 1,
+    books: {
+      connect: [{ id: 5129 }, { id: 4381 }, { id: 11 }],
+    },
+  },
+  {
+    name: "Classics",
+    userId: 1,
+    books: {
+      connect: [{ id: 391729 }, { id: 10799 }, { id: 46170 }],
+    },
+  },
+];
+
+const create = async (type: any, items: any) => {
+  for (const item of items) {
+    try {
+      await type.create({ data: item });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          // already exists - ignore
+          continue;
+        }
+      }
+      throw e;
+    }
+  }
+};
+
+const createLists = async () => {
+  listData.forEach(async (data, index) => {
+    const id = index + 1;
+    await prisma.list.upsert({
+      where: {
+        id: id,
+      },
+      update: {
+        ...data,
+      },
+      create: {
+        id: id,
+        ...data,
+      },
+    });
+  });
+};
+
+const main = async () => {
+  await create(prisma.user, users);
+  await create(prisma.book, books);
+  await createLists();
+};
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
