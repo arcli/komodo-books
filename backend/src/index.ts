fa6c666e1b66f5ed11888c6f40d80b78a9086d2b
Seth Ladygo <alx-git@arclight.industries>
2023-10-24 03:58:23 +0000

    Add CORS control for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,32 @@
 import { PrismaClient } from "@prisma/client";
-import express, { Express, Request, Response } from "express";
+import express from "express";
+import cors from "cors";
 
 const prisma = new PrismaClient();
-const app: Express = express();
+const app = express();
 const port = 8000;
 
+var corsOptions = {
+  origin: true, // allow all for dev/demo purpose
+  // for prod, could do something like:
+  // origin: [process.env.CORS_ALLOWED_URL, "http://localhost:3000"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("foo");
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
-app.get("/test", async (req, res) => {
-  const users = await prisma.user.findMany({
+app.get("/books/all", async (req, res) => {
+  const books = await prisma.book.findMany({
     // where: { published: true },
     // include: { author: true },
   });
-  res.json(users);
+  res.json(books);
 });
 
 app.listen(port, () => {
-  console.log(`server running at http://localhost:${port}`);
+  console.log(`server running at http://0.0.0.0:${port}`);
 });
